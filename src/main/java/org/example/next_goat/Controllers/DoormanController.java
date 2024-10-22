0adda7a3b56f0c12e944edcaccef3d8b25c0c630
ElMohaa <arzoukmohamed3@gmail.com>
2024-10-22 22:14:08 +0200

    Descripción breve de los cambios

---
+++ src/main/java/org/example/next_goat/Controllers/DoormanController.java
@@ -0,0 +1,30 @@
+package org.example.next_goat.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DoormanController {
+    @FXML
+    private void handleBackButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/Training.fxml"));
+            Parent training = loader.load();
+
+            Scene trainingScene = new Scene(training);
+
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(trainingScene);
+            window.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
